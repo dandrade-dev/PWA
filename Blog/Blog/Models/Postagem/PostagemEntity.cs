bc1cdff9008a393a75cd0f293abad871f671dc0c
sanoaddams <61031307+sanoaddams@users.noreply.github.com>
2020-02-20 19:36:21 -0300

    Atualizando a pasta Model

---
+++ Blog/Blog/Models/Postagem/PostagemEntity.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        public string Titulo;
+        public AutorEntity Autor;
+        public CategoriaEntity Categoria;
+    }
+}
